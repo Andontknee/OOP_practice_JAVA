bff0c204d0173664878dc6a514230748c9b7033e
AnthonyLCM <anthonysunway@gmail.com>
2025-01-27 23:47:55 +0800

    just testing, first acct class java

---
+++ Exercises On Classes/Account.java
@@ -0,0 +1,51 @@
+public class Account {
+    private String id;
+    private String name;
+    private int balance;
+
+    public Account(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Account(String id, String name, int balance) {
+        this.id = id;
+        this.name = name;
+        this.balance = balance;
+    }
+
+    public String getID() {
+        return id;
+    }
+    public void setID(String id) {
+        this.id = id;
+    }
+
+    public String getname() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
+
+    public int credit(int amount) {
+        this.balance += amount;
+        return this.balance;
+    }
+    public int debit(int amount){
+        if(amount <=this.balance) {
+            this.balance -=amount;
+        }else{
+
+        }
+        
+    }
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 "# OOP_practice_JAVA" 
+link to github practice : https://github.com/giakinh0823/java-programming-tutorial/tree/main
